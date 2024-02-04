831f84cbb93a2385d5fbdedb4b1db680aa4bfec3
Harshit Singh Tomar <harshit.nitmz@gmail.com>
2024-02-04 14:14:03 +0530

    fix: highlight chip border

--- src/components/UserChip.jsx
+++ src/components/UserChip.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
-import { useUserData } from '../contexts/UserContext'
-import { usersData } from './UsersList/usersData'
+import React from "react";
+import { useUserData } from "../contexts/UserContext";
+import { usersData } from "./UsersList/usersData";
 
-const UserChip = ({highLightedUser}) => {
-  const {selectedUsers,setSelectedUsers,setUsersList}=useUserData()
-    let removeUser=(id)=>{                                   
-     let remainingUsers=selectedUsers?.filter((user)=>{       //remove user
-           return user.id!==id
-     })
-     setSelectedUsers(remainingUsers)
-     let removedUser=usersData.find((user)=>(user.id===id))
-     setUsersList((prev)=>([...prev,removedUser]))   //update user list again while revoking
-    }
+const UserChip = ({ highLightedUser }) => {
+  const { selectedUsers, setSelectedUsers, setUsersList } = useUserData();
+  let removeUser = (id) => {
+    let remainingUsers = selectedUsers?.filter((user) => {
+      //remove user
+      return user.id !== id;
+    });
+    setSelectedUsers(remainingUsers);
+    let removedUser = usersData.find((user) => user.id === id);
+    setUsersList((prev) => [...prev, removedUser]); //update user list again while revoking
+  };
 
   return (
-    <div className='flex gap-2 max-w-max flex-wrap  '>
-      {selectedUsers?.map((user)=>{
-  
-        return  <div key={user.id} className={`flex bg-[#ccc] border-2 border-transparent border-solid gap-2 rounded-md py-1 px-[4px] max-w-max text-nowrap ${highLightedUser===user.id && 'border-red-500'} }`}>
-           <p className='text-sm'>{user.name}</p>
-           <span onClick={()=>removeUser(user.id)} className='font-bold text-red-500 cursor-pointer'>X</span>
-        </div>
+    <div className="flex gap-2 max-w-max flex-wrap  ">
+      {selectedUsers?.map((user) => {
+        return (
+          <div
+            key={user.id}
+            className={`flex bg-[#ccc] border-2  border-solid gap-2 rounded-md py-1 px-[4px] max-w-max text-nowrap ${
+              highLightedUser === user.id ? "border-red-500" : ""
+            }`}
+          >
+            <p className="text-sm">{user.name}</p>
+            <span
+              onClick={() => removeUser(user.id)}
+              className="font-bold text-red-500 cursor-pointer"
+            >
+              X
+            </span>
+          </div>
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default UserChip
+export default UserChip;
